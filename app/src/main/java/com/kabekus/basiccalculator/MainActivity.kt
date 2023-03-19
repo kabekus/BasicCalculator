c37a4056a3033ff5e958f51b068f95d3abbb0aa8
kabekus <kabekus.123@gmail.com>
2023-03-19 19:34:58 +0300

    First Commit

---
+++ app/src/main/java/com/kabekus/basiccalculator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kabekus.basiccalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("Hello Kotlin")
+    }
+}
